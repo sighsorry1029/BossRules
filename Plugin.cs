2c111206252d6becf210b498bad1db85c20ee027
blizz <sighsorry1029@gmail.com>
2026-08-07 11:44:22 +0900

    Release BossRules 1.0.6

--- Plugin.cs
+++ Plugin.cs
@@ -15,7 +15,7 @@ namespace BossRules;
 public sealed class BossRulesPlugin : BaseUnityPlugin
 {
     internal const string ModName = "BossRules";
-    internal const string ModVersion = "1.0.5";
+    internal const string ModVersion = "1.0.6";
     internal const string Author = "sighsorry";
     internal const string ModGUID = $"{Author}.{ModName}";
     internal const string AltarYamlFileName = $"{ModName}.altar.yml";
